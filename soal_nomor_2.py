67e98147570115a243e0b1a99338a05ff1a28eeb
trinanda <root@localhost.localdomain>
2018-11-17 15:23:04 +0700

    mengerjakan soal nomor 2

---
+++ soal_nomor_2.py
@@ -0,0 +1,18 @@
+import re
+
+def validate():
+    while True:
+        password = input("Enter a password: ")
+        if len(password) < 8:
+            print("Minimal karakter untuk password nya adalah 8")
+        elif re.search('[@#$%^&+=_.-]',password) is None:
+            print("Pola yang di masukan salah, mohon diulangi!")
+        elif re.search('[A-Z]', password) is None:
+            print("Pola yang di masukan salah, mohon diulangi!")
+        elif re.search('[a-z]', password) is None:
+            print("Pola yang di masukan salah, mohon diulangi!")
+        else:
+            print("Your password seems fine")
+            break
+
+validate()
