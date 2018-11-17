fed4295c316d30a224c8aead1496e4d966033520
trinanda <root@localhost.localdomain>
2018-11-17 20:45:48 +0700

    selesai

--- soal_nomor_1.py
+++ soal_nomor_1.py
@@ -1,4 +1,24 @@
-itemId = 12341822
-itemName = 'FGX Flannel Shirt'
-price = 195000
-availableColorAndSize = 
+import json
+
+
+def product():
+    itemId = 12341822
+    itemName = 'FGX Flannel Shirt'
+    price = 195000
+    availableColorAndSize = {
+        'color': {'blue-black': ['M', 'L', 'XL'],
+                  'black-white': ['L']}
+    }
+
+    freeShiping = False
+
+    with open('data.json', 'w') as fp:
+        json.dump(
+        {'itemId': itemId,
+         'itemName': itemName,
+         'price': price,
+         'availableColorAndSize': availableColorAndSize,
+         'freeShiping': freeShiping}, fp)
+
+if __name__ == "__main__":
+    product()
